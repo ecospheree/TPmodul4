f381adf997e2e106d30a65a19dba09e7e3775f0e
ecospheree <121998521+ecospheree@users.noreply.github.com>
2023-03-13 00:26:56 +0700

    menambahkan implementasi dengan table-driven

--- TPmodul4_1302210049/Program.cs
+++ TPmodul4_1302210049/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿public class KodePos
+{
+    public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja };
+    public static int getKodePos(Kelurahan KELURAHAN)
+    {
+        int[] kodepos = { 40266, 40287, 40256, 40287, 40286, 40272, 40274, 40273, 40272, 40274, 40273 };
+        return kodepos[(int)KELURAHAN];
+    }
+}
+
+
 
 
 
